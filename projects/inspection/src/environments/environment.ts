21a639ddfb384720686a2b87a3efaa91f62aa0a2
Asmaa Hassan <asmaahassanhamza1@gmail.com>
2024-09-10 11:23:25 +0300

    run app

---
+++ projects/inspection/src/environments/environment.ts
@@ -0,0 +1,36 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+const oAuthConfig = {
+  issuer: 'https://dev-auth-inspection.starwayseg.com/',
+  redirectUri: baseUrl,
+  clientId: 'Angular',
+  responseType: 'code',
+  scope:
+    'offline_access openid profile email phone AccountService  AdministrationService SaasService ',
+  requireHttps: true,
+};
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'Inspection',
+  },
+  oAuthConfig,
+  apis: {
+    default: {
+      url: 'http://dev-gateway-inspection.starwayseg.com',
+      rootNamespace: 'ELM.Inspection',
+    },
+    AbpAccountPublic: {
+      url: oAuthConfig.issuer,
+      rootNamespace: 'AbpAccountPublic',
+    },
+    ProductService: {
+      url: 'http://dev-gateway-inspection.starwayseg.com/',
+      rootNamespace: 'ELM.Inspection.ProductService',
+    },
+  },
+} as Environment;
